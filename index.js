1dac4a8107e69cdde175a3c5c615ff24fb61f40a
kimbela winston <kimbelawin84@gmail.com>
2023-06-21 17:16:16 -0500

    collerday

---
+++ index.js
@@ -0,0 +1,27 @@
+function Scooter(year, color, model) {
+    this.year = year;
+    this.color = color;
+    this.model = model;
+  }
+  //const myScooter = new Scooter(2022, 'Red', 'XYZ Model');
+  //console.log(myScooter.year);   
+  //console.log(myScooter.color);  
+  //console.log(myScooter.model);
+
+  function Driver(name, age, experience) {
+    this.name = name;
+    this.age = age;
+    this.experience = experience;
+  }
+  //const myDriver = new Driver('John Doe', 30, '5 years');
+  console.log(myDriver.name);       
+console.log(myDriver.age);        
+console.log(myDriver.experience);
+
+  function PickupLocation(address, city) {
+    this.address = address;
+    this.city = city;
+  }
+  //const myPickupLocation = new PickupLocation('123 Main St', 'New York');
+  //console.log(myPickupLocation.address);  
+  //console.log(myPickupLocation.city); 
